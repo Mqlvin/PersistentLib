f8553897c83d968b64e6d731dcb6d724ec7589e2
Mqlvin <pricklydotcactus@gmail.com>
2022-02-23 20:58:19 +0000

    Edit on Config class, added DatumPlaceholder.

--- src/main/me/henry/persistent/config/Config.java
+++ src/main/me/henry/persistent/config/Config.java
@@ -1,21 +1,23 @@
 package main.me.henry.persistent.config;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import main.me.henry.persistent.datum.Datum;
+import main.me.henry.persistent.datum.DatumPlaceholder;
 import main.me.henry.persistent.datum.DatumType;
 import main.me.henry.persistent.exceptions.InvalidDatumNameException;
 import main.me.henry.persistent.exceptions.InvalidDatumTypeException;
+import main.me.henry.persistent.exceptions.InvalidModifiersException;
 
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Objects;
+import java.lang.reflect.Modifier;
+import java.util.*;
 
 public abstract class Config {
+    private HashMap<String, ArrayList<DatumPlaceholder>> locations_names = new HashMap<>();
     public Config(String name, File location, ConfigFormat format) {
-        HashMap<String, ArrayList<String>> locations_names = new HashMap<>();
-
         for(Field field : this.getClass().getDeclaredFields()) {
             Datum fieldMeta;
             if(!field.isAnnotationPresent(Datum.class)) {
@@ -34,10 +36,24 @@ public abstract class Config {
                 if(!(field.getType().equals(int.class) || field.getType().equals(Integer.class))) {
                     throw new InvalidDatumTypeException("The datum type \"" + fieldMeta.type().getType().getName() + "\" cannot be applied to field \"" + field.getType().getName() + "\".");
                 }
-            } else {
-                if(!field.getType().equals(fieldMeta.type().getType())) {
+            } else if(fieldMeta.type().equals(DatumType.BOOLEAN)) {
+                if(!(field.getType().equals(boolean.class) || field.getType().equals(Boolean.class))) {
                     throw new InvalidDatumTypeException("The datum type \"" + fieldMeta.type().getType().getName() + "\" cannot be applied to field \"" + field.getType().getName() + "\".");
                 }
+            } else {
+                if (!field.getType().equals(fieldMeta.type().getType())) {
+                    throw new InvalidDatumTypeException("The datum type \"" + fieldMeta.type().getType().getName() + "\" cannot be applied to field \"" + field.getType().getName() + "\".");
+                }
+            }
+            ArrayList<String> modifiers = new ArrayList<>(Arrays.asList(Modifier.toString(field.getModifiers()).toLowerCase().split(" ")));
+            if(modifiers.contains("private")) {
+                throw new InvalidModifiersException("Declared datum \"" + fieldMeta.name() + " contains illegal modifier \"private\".");
+            } else if(!modifiers.contains("public")) {
+                throw new InvalidModifiersException("Declared datum \"" + fieldMeta.name() + "\" requires the \"public\" modifier.");
+            } else if(!modifiers.contains("static")) {
+                throw new InvalidModifiersException("Declared datum \"" + fieldMeta.name() + "\" requires the \"static\" modifier.");
+            } else if(modifiers.contains("final")) {
+                throw new InvalidModifiersException("Declared datum \"" + fieldMeta.name() + "\" contains illegal modifier \"final\".");
             }
 
 
@@ -45,7 +61,7 @@ public abstract class Config {
             if(!locations_names.containsKey(fieldMeta.location().toLowerCase())) {
                 locations_names.put(fieldMeta.location().toLowerCase(), new ArrayList<>());
             }
-            locations_names.get(fieldMeta.location().toLowerCase()).add(fieldMeta.name());
+            locations_names.get(fieldMeta.location().toLowerCase()).add(new DatumPlaceholder(fieldMeta, field));
         }
 
 
@@ -56,14 +72,56 @@ public abstract class Config {
         System.out.println(configLocation);
         System.out.println(configLocation.toPath());
         if(!configLocation.exists()) {
-            // make config default values here
+            save();
         }
 
 
     }
 
     public void save() {
+        JsonObject saveObject = new JsonObject();
 
+        for(String loc : locations_names.keySet()) {
+            ArrayList<String> splitPath = new ArrayList<>(Arrays.asList(loc.split("/")));
+            splitPath.removeIf(s -> s == null || s.equals("") || s.equals(" "));
+            Collections.reverse(splitPath);
+            boolean inMain = true;
+            String locName = "";
+            if(splitPath.size() != 0) {
+                locName = splitPath.get(0);
+                splitPath.remove(0);
+                inMain = false;
+            }
+
+            ArrayList<DatumPlaceholder> datums = locations_names.get(loc);
+            JsonObject object = new JsonObject();
+            for(DatumPlaceholder datum : datums) {
+                try {
+                    if(inMain) {
+                        saveObject.addProperty(datum.getMeta().name(), String.valueOf(datum.getField().get(datum.getField().getType())));
+                    } else {
+                        object.addProperty(datum.getMeta().name(), String.valueOf(datum.getField().get(datum.getField().getType())));
+                    }
+                } catch(IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if(inMain) {
+                continue;
+            }
+
+            JsonObject newObj = object;
+            for(String f : splitPath) {
+                JsonObject n = new JsonObject();
+                n.add(f, newObj);
+                newObj = n;
+                System.out.println(newObj);
+            }
+            // merge existing
+            saveObject.add(locName, newObj);
+        }
+        System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(saveObject));
     }
 
     public void load() {
--- src/main/me/henry/persistent/datum/DatumPlaceholder.java
+++ src/main/me/henry/persistent/datum/DatumPlaceholder.java
@@ -1,2 +1,21 @@
-package main.me.henry.persistent.datum;public class DatumPlaceholder {
+package main.me.henry.persistent.datum;
+
+import java.lang.reflect.Field;
+
+public class DatumPlaceholder {
+    private Datum meta;
+    private Field field;
+
+    public DatumPlaceholder(Datum datum, Field field) {
+        this.meta = datum;
+        this.field = field;
+    }
+
+    public Datum getMeta() {
+        return meta;
+    }
+
+    public Field getField() {
+        return field;
+    }
 }
